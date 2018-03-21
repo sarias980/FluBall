c1e845671aa76667c7a0b062e43675c019280670
Sarias <53831285s@iespoblenou.org>
2018-03-21 19:01:22 +0100

    Navegacion basica

--- app/src/main/java/com/example/sergi/fluball/MainActivityFragment.java
+++ app/src/main/java/com/example/sergi/fluball/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.sergi.fluball;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,18 +21,26 @@ import java.util.Arrays;
 public class MainActivityFragment extends Fragment {
 
     private static final int RC_SIGN_IN = 123;
+    private Intent intent;
+    private TextView name;
 
     public MainActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        name = (TextView) view.findViewById(R.id.nombreUsuario);
+        Button juego = (Button) view.findViewById(R.id.btnJuegos);
+        Button dispositivos = (Button) view.findViewById(R.id.btnDispositivos);
+        Button opciones = (Button) view.findViewById(R.id.btnOpciones);
+        Button help = (Button) view.findViewById(R.id.btnHelp);
+
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
-
+            updateNameUser(name,auth.getCurrentUser().getEmail());
         } else {
             startActivityForResult(
                     AuthUI.getInstance()
@@ -40,6 +52,44 @@ public class MainActivityFragment extends Fragment {
                     RC_SIGN_IN);
         }
 
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnJuegos:
+                        Log.d("BTN", "Juego");
+                        intent = new Intent(getContext(), JuegosMainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.btnDispositivos:
+                        Log.d("BTN", "Dispo");
+                        intent = new Intent(getContext(), DispositivosMainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.btnOpciones:
+                        Log.d("BTN", "Opc");
+                        intent = new Intent(getContext(), ConfigMainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.btnHelp:
+                        Log.d("BTN", "Help");
+                        intent = new Intent(getContext(), HelpMainActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        };
+
+        juego.setOnClickListener(listener);
+        dispositivos.setOnClickListener(listener);
+        opciones.setOnClickListener(listener);
+        help.setOnClickListener(listener);
+
+
         return view;
+    }
+
+    private void updateNameUser(TextView name, String user) {
+        name.setText(user);
     }
 }
